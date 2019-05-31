664742d5782c8cabb6408fcfe335962e665d9930
Marco <marco.messa3@gmail.com>
2019-05-31 15:28:56 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+class Toggler {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.itemsToToggle = document.querySelectorAll('[data-toggle]');
+        
+        this.addEvents(this.itemsToToggle);
+    }
+
+    addEvents(items) {
+        items.forEach(item => {
+            item.addEventListener('click', () => {
+                const ids = item.dataset.toggle.split(" ");
+                ids.forEach(id => {
+                    const el = document.getElementById(id);
+                    const classes = item.dataset.toggler.split(" ")
+                    classes.forEach( className => {
+                        el.classList.toggle(className)
+                    })
+                })
+            })
+        });
+    }
+}
+
+export default Toggler;
